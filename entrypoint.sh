43ae75c09c963d2c9212201fc1fa34359e19e8b4
Don Michael Graben <don.michael.graben@svt.se>
2023-05-16 19:22:39 +0200

    added exludes

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,7 +29,7 @@ chmod 644 $SSH_KNOWN_HOSTS_PATH
 
 echo "$BRANCH" > environment
 
-EXCLUDES=
+EXCLUDES={".*",".*/",".github",".git",".gitignore"}
 
 rsync -e "ssh -v -p 22 -i $SSH_PRIVATE_KEY_PATH -o UserKnownHostsFile=$SSH_KNOWN_HOSTS_PATH" --verbose --delete-excluded --exclude=$EXCLUDES --filter="protect robots.txt" --filter="protect .htaccess" -a $SOURCE_PATH $TARGET_PATH
 
